4d44b21a6980b0653cf9d0538dc210098027683d
HaruDev <sarankhtn@gmail.com>
2024-10-03 13:28:10 +0700

    feat : add all the assignments

--- ex-1.js
+++ ex-1.js
@@ -1,1 +1,24 @@
 // Start coding here
+function add (a, b) {
+    return a + b ;
+}
+const resultAdd = add(10, 5);
+console.log(resultAdd);
+
+function subtract (a, b) {
+    return a - b ;
+}
+const resultSubtact = subtract(10, 5)
+console.log(resultSubtact);
+
+function multiple (a, b){
+    return a * b ;
+}
+const resultMultiply = multiple(10, 5);
+console.log(resultMultiply);
+
+function divide (a, b){
+    return a / b ;
+}
+const resultDivide = divide(10, 5);
+console.log(resultDivide)
